02fb58d388faa3d1eac6ed2947715621f7b13341
karthik <karthik.vbdon88@gmail.com>
2023-04-30 13:22:01 +1200

    updated timeout

--- script.js
+++ script.js
@@ -6,26 +6,12 @@ function reverseString() {
   let userInputValue = userInput.value;
   let reversedArray = userInputValue.split("").reverse();
   let reversedString = reversedArray.join("");
-  return reversedString;
+  display.textContent = "Reversed String is: " + reversedString;
 }
 
-console.log(reverseString());
-
 btnReverse.addEventListener("click", () => {
-  if (reverseString() !== "") {
-    let count = 2;
-    display.textContent = count;
-
-    let intervalId = setInterval(() => {
-      display.textContent = count;
-      count--;
-      if (count > 0) {
-        display.textContent = count;
-      } else {
-        clearInterval(intervalId);
-        display.textContent = "Reversed String is : " + reverseString();
-      }
-    }, 1000);
+  if (userInput.value !== "") {
+    setTimeout(reverseString, 2000);
   } else {
     display.textContent = "Please enter a valid String";
   }
